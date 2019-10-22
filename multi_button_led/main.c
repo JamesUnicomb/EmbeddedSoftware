64289682928cdf040e18ca5bd7c8f595c5d58557
jamesunicomb <james.unicomb@gmail.com>
2019-10-23 08:50:58 +1100

    .

--- multi_button_led/main.c
+++ multi_button_led/main.c
@@ -5,33 +5,33 @@ void main(void)
     WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
 
     // LEDS
-    P2DIR |= BIT5;                  // blue LED
-    P2DIR |= BIT4;                  // red LED
-    P1DIR |= BIT5;                  // green LED
+    P4DIR |= BIT0;                  // blue LED
+    P3DIR |= BIT7;                  // red LED
+    P8DIR |= BIT2;                  // green LED
 
     while(1)
     {
-        if (P4IN & BIT0) {          // yellow button
-            P2OUT |= BIT4;
-            P1OUT |= BIT5;
+        if (P2IN & BIT5) {          // red button
+            P3OUT |= BIT7;
         }
-        else if (P4IN & BIT3) {     // red button
-            P2OUT |= BIT4;
+        else if (P2IN & BIT4) {     // yellow button
+            P3OUT |= BIT7;
+            P8OUT |= BIT2;
         }
-        else if (P3IN & BIT7) {     // green button
-            P1OUT |= BIT5;
+        else if (P1IN & BIT5) {     // green button
+            P8OUT |= BIT2;
         }
-        else if (P8IN & BIT2) {     // blue button
-            P2OUT |= BIT5;
+        else if (P1IN & BIT4) {     // blue button
+            P4OUT |= BIT0;
         }
-        else if (P8IN & BIT1) {     // purple button
-            P2OUT |= BIT4;
-            P2OUT |= BIT5;
+        else if (P1IN & BIT3) {     // purple button
+            P3OUT |= BIT7;
+            P4OUT |= BIT0;
         }
         else {
-            P1OUT &= ~BIT5;
-            P2OUT &= ~BIT4;
-            P2OUT &= ~BIT5;
+            P4OUT &= ~BIT0;
+            P3OUT &= ~BIT7;
+            P8OUT &= ~BIT2;
         }
     }
 }
